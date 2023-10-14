2586787392d52cbf3d14115c20f5977b6a6fa2e6
Maria Flores <mariafd1212@gmail.com>
2023-10-13 21:26:13 -0300

    Forms y Icons

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { IconsComponent } from './pages/icons/icons.component';
+import { IconsModule } from './pages/icons/icons.module';
+import { FormsModule } from '@angular/forms';
+import { FormsComponent } from './pages/forms/forms.component';
+
 
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent, FormsComponent
   ],
   imports: [
     CommonModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatCardModule,
+    MatButtonModule,
+    IconsModule,
+    FormsModule
   ],
   exports: [DashboardComponent],
 })
---
+++ src/app/dashboard/pages/icons/icons.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IconsComponent } from './icons.component';
+import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+
+
+@NgModule({
+  declarations: [
+    IconsComponent
+  ],
+  imports: [
+    CommonModule, MatCardModule, MatButtonModule, MatIconModule
+  ],
+  exports: [
+    IconsComponent
+  ]
+})
+export class IconsModule { }
